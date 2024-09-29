171e22e425f81f9fd11697082a9014d35d6c844d
hussamkhatib <hussamkhatib20@gmail.com>
2024-09-30 01:40:56 +0400

    :memo:: remove OSS brag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,23 +15,5 @@
 				rel="noopener noreferrer">ShipperCRM</a
 			>
 		</p>
-		<p>
-			I mainly work with React and TypeScript, and I'm currently learning Svelte. I love
-			contributing to open source. Some of the projects I have contributed to include <a
-				href="https://github.com/calcom/cal.com/pulls?q=is%3Apr+author%3Ahussamkhatib+is%3Amerged"
-				target="_blank"
-				rel="noopener noreferrer">Cal.com</a
-			>,
-			<a
-				href="https://github.com/codesandbox/sandpack/pull/490"
-				target="_blank"
-				rel="noopener noreferrer">Sandpack</a
-			>, and
-			<a
-				href="https://github.com/search?q=is%3Apr+author%3Ahussamkhatib+-user%3Ahussamkhatib+-repo%3Acalcom%2Fcal.com+-org%3Ashoonyatech+-org%3AshipperCRM+-repo%3Acodesandbox%2Fsandpack+-repo%3ADharmarajX24%2Fnodesports+is%3Amerged&type=pullrequests"
-				target="_blank"
-				rel="noopener noreferrer">many more</a
-			>.
-		</p>
 	</section>
 </div>
